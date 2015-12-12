6ed985ba24c14f70c7eebfa0a7ad8686c1b572dd
Bogdan Derevyaga <Bogdan Derevyaga>
2015-12-12 18:25:02 +0200

    AI

--- js/controllers.js
+++ js/controllers.js
@@ -18,27 +18,73 @@ function IndexController($scope, $http) {
   	});
 
 	$scope.getRandomCard = function() {
+		if($scope.deck.length == 0){
+			return null;
+		}
 		var cardIndex = getRandomInt(0, $scope.deck.length - 1);
-		var colorIndex = getRandomInt(0, 3);
-		return {
+		var colorIndex = getRandomInt(0, $scope.deck[cardIndex].img.length - 1);
+		var card = {
 			"img": $scope.deck[cardIndex].img[colorIndex],
 			"point": $scope.deck[cardIndex].point
+		};
+
+		$scope.deck[cardIndex].img.splice(colorIndex, 1);
+		if($scope.deck[cardIndex].img.length == 0){
+			$scope.deck.splice(cardIndex, 1);
 		}
+		return card;
 	}
 
 	$scope.getCards = function(count, hand) {
 		var pointSumm = 0;
 		for (var i = 0; i < count; i++) {
 			var cardData = $scope.getRandomCard();
-			hand.push(cardData);
-			pointSumm += cardData.point;
+			if(cardData){
+				hand.push(cardData);
+				pointSumm += cardData.point;
+			}
 		}
 		return pointSumm;
 	}
 
 	$scope.addCard = function()	{
 		var cardData = $scope.getRandomCard();
-		$scope.userCards.push(cardData);
-		$scope.userPoint += cardData.point;
+		if(cardData){
+			$scope.userCards.push(cardData);
+			$scope.userPoint += cardData.point;
+		}else{
+			console.log("Карти закончились");
+		}
+	}
+
+	$scope.addDealerCard = function()	{
+		if($scope.dealerPoint >= 21){
+			return false;
+		}
+		var cardData = $scope.getRandomCard();
+		if(cardData){
+			$scope.dealerCards.push(cardData);
+			$scope.dealerPoint += cardData.point;
+			return true;
+		}else{
+			console.log("Карти закончились");
+			return false;
+		}
+	}
+
+	$scope.endGame = function()	{
+		var status = true;
+		while(status){
+			status = $scope.addDealerCard();
+		}
+		if($scope.userPoint > 21 && $scope.dealerPoint > 21 || $scope.userPoint == $scope.dealerPoint){
+			console.log("Ничя");
+			return false;
+		}
+		if($scope.userPoint > $scope.dealerPoint && $scope.userPoint <= 21 || $scope.dealerPoint > 21){
+			console.log("Вы виграли");
+		}else{
+			console.log("Дилер вииграл");		
+		}
 	}
 }
