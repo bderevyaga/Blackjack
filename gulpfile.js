7760752a15451d5b11e8aa630ddb08532b28c635
bderevyaga <bogdan.derevyaga@gmail.com>
2025-10-05 21:42:02 +0300

    gulp updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,47 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    livereload = require('gulp-livereload'),
-	autoprefixer = require('gulp-autoprefixer'),
-	connect = require('gulp-connect'),
-	notify = require("gulp-notify");
+const {src, dest, watch, parallel} = require('gulp');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const connect = require('gulp-connect');
+const notify = require('gulp-notify');
 
-gulp.task('default', ['connect', 'watch']);	
-	
-gulp.task('connect', function() {
-  connect.server({
-    root: '',
-    livereload: true
-  });
-});
+function connectServer(done) {
+    connect.server({
+        root: '.',
+        livereload: true
+    });
+    done();
+}
 
-gulp.task('html', function () {
-  gulp.src('*.html')
-    .pipe(connect.reload());
-});
-	
-gulp.task('less', function() {
-  gulp.src('less/*.less')
-    .pipe(less())
-	.pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+function html() {
+    return src('*.html')
+        .pipe(connect.reload());
+}
+
+function js() {
+    return src('js/*.js')
+        .pipe(connect.reload());
+}
+
+function styles() {
+    return src('less/*.less')
+        .pipe(less())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
             cascade: false
         }))
-    .pipe(gulp.dest('css'))
-	.pipe(notify("CSS"))
-	.pipe(connect.reload());
-});
- 
-gulp.task('watch', function() {
-  gulp.watch('less/*.less', ['less']);
-  gulp.watch('*.html', ['html']);
-});
+        .pipe(dest('css'))
+        .pipe(notify('CSS'))
+        .pipe(connect.reload());
+}
 
+function watchFiles() {
+    watch('less/*.less', styles);
+    watch('js/*.js', js);
+    watch('*.html', html);
+}
+
+exports.html = html;
+exports.less = styles;
+exports.watch = watchFiles;
+exports.connect = connectServer;
+exports.default = parallel(connectServer, watchFiles);
--- js/controllers.js
+++ js/controllers.js
@@ -1,122 +1,124 @@
-app.controller("IndexController", IndexController);
+app.controller('IndexController', IndexController);
 
 function getRandomInt(min, max) {
- 	return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 // Інтелект ділера
 function dealerAI(min, max, point) {
- 	var ai = getRandomInt(min, max);
- 	if(ai >= ( 21 - point )){
- 		return false;
- 	}
- 	return true;
+    const ai = getRandomInt(min, max);
+
+    return ai < (21 - point);
 }
+
 function IndexController($scope, $http) {
-	var gameStatus = true;
-	if(!localStorage.getItem('userMoney')){
-		localStorage.setItem('userMoney', 100);		
-	}
-	$scope.userMoney = localStorage.getItem('userMoney');
-	//Поставити гроші
-	$scope.addMoney = function(){
-		if(gameStatus){
-			if($scope.userMoney > 0){
-				$scope.userMoney -= 10;
-				$scope.userAnte += 10;
-			}
-			localStorage.setItem('userMoney', $scope.userMoney);
-		}
-	}
-	//Нова гра 
-	$scope.newGame = function()	{
-		gameStatus = true;
-		$scope.openDealerCards = false;
-		$scope.userAnte = 0;
-		$scope.userPoint = 0;
-		$scope.dealerPoint = 0;
-		$scope.dealerCards = [];
-		$scope.userCards = [];
-	  	$scope.deck = [];
-	  	$scope.userMoney -= 10;
-		$scope.userAnte += 10;
-		localStorage.setItem('userMoney', $scope.userMoney);
+    let gameStatus = true;
+    if (!localStorage.getItem('userMoney')) {
+        localStorage.setItem('userMoney', '100');
+    }
+    $scope.userMoney = localStorage.getItem('userMoney');
+    // Поставити гроші
+    $scope.addMoney = function () {
+        if (gameStatus) {
+            if ($scope.userMoney > 0) {
+                $scope.userMoney -= 10;
+                $scope.userAnte += 10;
+            }
+            localStorage.setItem('userMoney', $scope.userMoney);
+        }
+    };
+    // Нова гра
+    $scope.newGame = function () {
+        gameStatus = true;
+        $scope.openDealerCards = false;
+        $scope.userAnte = 0;
+        $scope.userPoint = 0;
+        $scope.dealerPoint = 0;
+        $scope.dealerCards = [];
+        $scope.userCards = [];
+        $scope.deck = [];
+        $scope.userMoney -= 10;
+        $scope.userAnte += 10;
+        localStorage.setItem('userMoney', $scope.userMoney);
 
-	  	$http.get("db/deck.json").success(function(response) {
-	  		$scope.deck = response;
-	  		$scope.userPoint = $scope.getCards(2, $scope.userCards);
-	  		$scope.dealerPoint = $scope.getCards(2, $scope.dealerCards);
-	  	});
-	}
+        $http.get('db/deck.json').success(function (response) {
+            $scope.deck = response;
+            $scope.userPoint = $scope.getCards(2, $scope.userCards);
+            $scope.dealerPoint = $scope.getCards(2, $scope.dealerCards);
+        });
+    };
 
-	$scope.newGame();
-	//Береться з колоди рандомна карта
-	$scope.getRandomCard = function() {
-		var cardIndex = getRandomInt(0, $scope.deck.length - 1);
-		var colorIndex = getRandomInt(0, $scope.deck[cardIndex].img.length - 1);
-		var card = {
-			"img": $scope.deck[cardIndex].img[colorIndex],
-			"point": $scope.deck[cardIndex].point,
-		};
+    $scope.newGame();
+    // Береться з колоди рандомна карта
+    $scope.getRandomCard = function () {
+        const cardIndex = getRandomInt(0, $scope.deck.length - 1);
+        const colorIndex = getRandomInt(0, $scope.deck[cardIndex].img.length - 1);
+        const card = {
+            'img': $scope.deck[cardIndex].img[colorIndex],
+            'point': $scope.deck[cardIndex].point
+        };
 
-		$scope.deck[cardIndex].img.splice(colorIndex, 1);
-		if($scope.deck[cardIndex].img.length == 0){
-			$scope.deck.splice(cardIndex, 1);
-		}
-		return card;
-	}
-	//Видається декілька карт 
-	$scope.getCards = function(count, hand) {
-		var pointSumm = 0;
-		for (var i = 0; i < count; i++) {
-			var cardData = $scope.getRandomCard();
-			hand.push(cardData);
-			pointSumm += cardData.point;
-		}
-		return pointSumm;
-	}
-	//Користувач бере карту
-	$scope.addCard = function()	{
-		if(gameStatus){
-			var cardData = $scope.getRandomCard();
-			$scope.userCards.push(cardData);
-			$scope.userPoint += cardData.point;
-			if($scope.userPoint > 21){
-				$scope.endGame();
-			}
-		}
-	}
-	//Ділер бере карту
-	$scope.addDealerCard = function()	{
-		if($scope.dealerPoint > 10){
-			var aiStatus = dealerAI(2, 11, $scope.dealerPoint);
-			if(!aiStatus){
-				return false;
-			}
-		}
-		var cardData = $scope.getRandomCard();
-		$scope.dealerCards.push(cardData);
-		$scope.dealerPoint += cardData.point;
-		return true;
-	}
-	//Ділер набирає карти, відкрити карти і вивисти результат гри
-	$scope.endGame = function()	{
-		var status = true;
-		if(gameStatus){
-			gameStatus = false;
-			$scope.openDealerCards = true;
-			while(status){
-				status = $scope.addDealerCard();
-			}
-			if($scope.userPoint > 21 && $scope.dealerPoint > 21 || $scope.userPoint == $scope.dealerPoint){
-				$scope.status = "Нічя";
-				$scope.userMoney += $scope.userAnte;	
-			}else if($scope.userPoint > $scope.dealerPoint && $scope.userPoint <= 21 || $scope.dealerPoint > 21){
-				$scope.status = "Ви виграли";
-				$scope.userMoney += ($scope.userAnte * 2);	
-			}else{
-				$scope.status = "Ділер виграв";		
-			}
-			localStorage.setItem('userMoney', $scope.userMoney);
-		}
-	}
+        $scope.deck[cardIndex].img.splice(colorIndex, 1);
+        if ($scope.deck[cardIndex].img.length === 0) {
+            $scope.deck.splice(cardIndex, 1);
+        }
+        return card;
+    };
+    // Видається декілька карт
+    $scope.getCards = function (count, hand) {
+        let pointSum = 0;
+        for (let i = 0; i < count; i++) {
+            const cardData = $scope.getRandomCard();
+            hand.push(cardData);
+            pointSum += cardData.point;
+        }
+        return pointSum;
+    };
+    // Користувач бере карту
+    $scope.addCard = function () {
+        if (gameStatus) {
+            const cardData = $scope.getRandomCard();
+            $scope.userCards.push(cardData);
+            $scope.userPoint += cardData.point;
+            if ($scope.userPoint > 21) {
+                $scope.endGame();
+            }
+        } else {
+            $scope.newGame();
+        }
+    };
+    // Ділер бере карту
+    $scope.addDealerCard = function () {
+        if ($scope.dealerPoint > 10) {
+            const aiStatus = dealerAI(2, 11, $scope.dealerPoint);
+            if (!aiStatus) {
+                return false;
+            }
+        }
+        const cardData = $scope.getRandomCard();
+        $scope.dealerCards.push(cardData);
+        $scope.dealerPoint += cardData.point;
+        return true;
+    };
+    // Ділер набирає карти, відкрити карти і вивисти результат гри
+    $scope.endGame = function () {
+        let status = true;
+        if (gameStatus) {
+            gameStatus = false;
+            $scope.openDealerCards = true;
+            while (status) {
+                status = $scope.addDealerCard();
+            }
+            if ($scope.userPoint > 21 && $scope.dealerPoint > 21 || $scope.userPoint === $scope.dealerPoint) {
+                $scope.status = 'Нічя';
+                $scope.userMoney += $scope.userAnte;
+            } else if ($scope.userPoint > $scope.dealerPoint && $scope.userPoint <= 21 || $scope.dealerPoint > 21) {
+                $scope.status = 'Ви виграли!';
+                $scope.userMoney += ($scope.userAnte * 2);
+            } else {
+                $scope.status = 'Дилер виграв';
+            }
+            localStorage.setItem('userMoney', $scope.userMoney);
+        }
+    };
 }
